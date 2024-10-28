c988ea5fbba54acadf0cf3b246ba973af477774f
unknown <2btd14@IESRAMONLLULL.LOCAL>
2024-10-28 12:07:31 +0100

    segundo commit

--- juego.py
+++ juego.py
@@ -1,4 +1,3 @@
-from carta import Carta
 from baraja import Baraja
 from mano import Mano
 
@@ -10,3 +9,23 @@ class Juego:
         manoJugador = Mano()
         manoJugador.añadirCarta(self.baraja.sacarCarta())
         print(f"Tu mano es {manoJugador.cartas}")
+        print(f"Total: {manoJugador.valor}")
+        while manoJugador.valor < 21:
+            accion = input("¿Quieres PEDIR carta o PASAR?")
+            if accion == "pedir":
+                manoJugador.añadirCarta(self.baraja.sacarCarta())
+                print(f"Tu mano es {manoJugador.cartas}")
+                print(f"Total: {manoJugador.valor}")
+            else:
+                print(f"Tu puntuacion final es {manoJugador.valor}")
+                return
+        if manoJugador.valor == 21:
+            print("ENHORABUENA")
+        elif manoJugador.valor < 21:
+            print("No has llegado ha 21")
+        else:
+            print("Te has pasado")
+if __name__ == '__main__':
+    print("JUEGO DE 21")
+    juego = Juego()
+    juego.jugar()
